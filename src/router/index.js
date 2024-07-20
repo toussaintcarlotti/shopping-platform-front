4f51f884c960d3ed253294d5dcd4d1630521c2ec
Toussaint CARLOTTI <toussaint.carlotti@gmail.com>
2024-07-20 08:16:01 +0200

    Correction de l'affichage du panier

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,6 @@ import ProductsView from "@/views/ProductsView.vue";
 import {useAuthStore} from "@/stores/authStore.js";
 import LoginView from "@/views/auth/loginView.vue";
 import RegisterView from "@/views/auth/RegisterView.vue";
-import {useCartStore} from "@/stores/cartStore.js";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
